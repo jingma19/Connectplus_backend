f66595de9449f2fc97ae52988142cc23da03e447
陈金然陈金然 <chenjinranchenjinran@chenjinranchenjinrans-Mac.local>
2020-04-20 11:29:39 -0700

    Added new model news for home page

--- ConnectPlus/models.py
+++ ConnectPlus/models.py
@@ -82,6 +82,7 @@ class Healthlog(models.Model):
 
 class News(models.Model):
     content = models.CharField(max_length = 255, blank = True, null = True)
+    detail = models.CharField(max_length = 255, blank = True, null = True)
     created_at = models.DateTimeField(blank = True, null = True)
     created_by = models.ForeignKey(User, blank = True, null = True, related_name = 'log_user_create', on_delete=models.CASCADE)
     shared_with = models.ForeignKey(User, blank = True, null = True, related_name = 'log_user_share', on_delete=models.CASCADE)
